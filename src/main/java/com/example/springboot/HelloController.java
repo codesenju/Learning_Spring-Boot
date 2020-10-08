7f132bc995cff85652d3f04e62b6be194495fb91
5367204 <lehlogonolo.masubelele@fnb.co.za>
2020-10-09 00:30:49 +0200

    Edited from vscode

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Bamboo!";
+		return "Hello from Bamboo!";
 	}
 
 }
--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -25,6 +25,6 @@ public class HelloControllerTest {
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Greetings from Bamboo!")));
+				.andExpect(content().string(equalTo("Hello from Bamboo!")));
 	}
 }
